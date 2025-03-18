c15532cb33c0ec9bcd0429103c54ef4d373b250c
saulish <saul.sanchez8791@alumnos.udg.mx>
2025-03-18 13:06:12 -0600

    Controlador de cisco añadido

---
+++ controladores/cisco.py
@@ -0,0 +1,63 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def getOfertasCisco(driver,indice,ofertas):
+    #url="https://jobs.cisco.com/jobs/SearchJobs/internship?21178=%5B102674%5D&21178_format=6020&21179=%5B12229585%5D&21179_format=6021&listFilterMode=1"
+    url="https://jobs.cisco.com/jobs/SearchJobs/internship?21178=%5B102674%5D&21178_format=6020&21179=%5B12229443%5D&21179_format=6021&listFilterMode=1"
+    driver.get(url)
+    # Espera a que cargue el contenedor con los listados de ofertas
+    print("Esperando a que carguen las ofertas...")
+    ofertasTmp=[]
+    wait = WebDriverWait(driver, 30)
+    # Espera a que el contenedor esté presente en el DOM
+    job_results_container = wait.until(EC.presence_of_element_located((By.ID, "content")))
+
+    rows = job_results_container.find_elements(By.TAG_NAME, "tr")
+    if rows[1].text=="No results":
+        print(f"Se encontraron 00 ofertas de empleo en Cisco")
+        return indice
+    # Iteramos sobre cada oferta y extraemos la información
+    for index, row in enumerate(rows, start=1):
+        if index==1:
+            continue
+        try:
+            # Extraer el título del trabajo (por ejemplo, en un <h2> con clase 'job-title')
+            columnas = row.find_elements(By.TAG_NAME, "td")
+            
+            job_title=columnas[0].text
+            job_location=columnas[3].text
+            if job_location !="Guadalajara, Mexico":
+                continue
+            job_link = columnas[0].find_element(By.TAG_NAME, "a").get_attribute("href")
+
+
+
+
+            oferta="\nEmpresa: Cisco"+"\n"+"  Título: "+job_title+"\n"+"  Ubicación: "+job_location+"\n"+"  Link: "+job_link
+            ofertasTmp.append(oferta)
+            """
+            
+            print(f"Oferta {index}:")
+            print(f"  Título: {job_title}")
+            print(f"  Ubicación: {job_location}")
+            print(f"  Link: {job_link}")
+            print("-" * 50)
+            """
+        except Exception as e:
+            print(f"Error extrayendo datos de la oferta {index}: {e}")
+    # Espera un poco antes de cerrar para observar resultados (opcional)
+    print(f"Se encontraron {len(ofertasTmp)} ofertas de empleo en Cisco")
+    for i, oferta in enumerate(ofertasTmp):
+        oferta="Oferta: "+str(i+indice+1)+oferta
+        ofertas.append(oferta)
+
+    time.sleep(5)
+    
+    return indice+len(ofertasTmp)
+
+
+    
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from controladores.intel import getOfertasIntel
 from controladores.oracle import getOfertasOracle
 from controladores.hp import getOfertasHP
 from controladores.ibm import getOfertasIBM
+from controladores.cisco import getOfertasCisco
 from procesamiento import procesarOfertas
 
 options = webdriver.ChromeOptions()
@@ -13,12 +14,19 @@ driver = webdriver.Chrome(options=options)
 if __name__ == "__main__":
     index=int(0)
     ofertas=[]
-    index = getOfertasOracle(driver, index, ofertas)
+    index = getOfertasCisco(driver, index, ofertas)
     index=getOfertasIBM(driver, index, ofertas)
     index = getOfertasIntel(driver, index, ofertas)
     index = getOfertasHP(driver, index, ofertas)
+    index = getOfertasOracle(driver, index, ofertas)
+    #C3
+    #JABIL
+    #MICROSOFT
+    #Bosch
+    #CompuTrabajo
     print(f"Se encontraron {len(ofertas)} ofertas en total.")
     procesarOfertas(ofertas)
     """
+
     """
     driver.quit()
